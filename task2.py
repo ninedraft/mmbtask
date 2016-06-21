146e31ee095bb466bbffc9ea9d2f0164382d81df
Павел Петрухин < >
2016-06-21 18:39:26 +0300

    i think i fixed a bug...

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
-from math import sqrt, pow, atan, sin, ceil
+from math import sqrt, pow, atan, sin, ceil, pi
 import time
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -6,3 +6,20 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 import progressbar
+
+hx = 0.01
+MinX = 0.0
+MaxX = pi 
+Nx = ceil((MaxX - MinX)/hx)
+
+hy = 0.01
+MinY = 0.0
+MaxY = 2
+Ny = ceil((MaxY - MinY)/hy)
+
+tau = 0.01
+MinT = 0.0
+MaxT = 2
+Nt = ceil((MaxT - MinT)/tau)
+
+w = np.zeros((Nx, Ny))
